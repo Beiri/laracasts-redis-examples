fda144c64c1c7379a82129c986554a8360831aab
Beiri Alife <beiri.al@outlook.com>
2024-09-22 17:26:28 -0300

    Ep 03 - Trending Articles with Sorted Sets

--- routes/web.php
+++ routes/web.php
@@ -1,27 +1,22 @@
 <?php
 
+use App\Article;
 use Illuminate\Support\Facades\Redis;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
-Route::get('videos/{id}', function ($id) {
-    $downloads = Redis::get("videos.{$id}.downloads");
+Route::get('articles/trending', function () {
+    $trending = Redis::zrevrange('trending_articles', 0, 2);
 
-    return view('welcome')->withDownloads($downloads);
+    $trending = Article::hydrate(
+        array_map('json_decode', $trending)
+    );
+
+    dd($trending);
 });
 
-Route::get('videos/{id}/download', function ($id) {
-    Redis::incr("videos.{$id}.downloads");
+Route::get('articles/{article}', function (Article $article) {
+    Redis::zincrby('trending_articles', 1, $article);
 
-    return back();
+    return $article;
 });
