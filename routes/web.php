fafe3c0620d5d4dbe891f41da500b5703d971619
Beiri Alife <beiri.al@outlook.com>
2024-09-23 18:49:16 -0300

    Ep 05 - Caching with Redis

--- routes/web.php
+++ routes/web.php
@@ -1,28 +1,11 @@
 <?php
 
+use App\Article;
 use Illuminate\Support\Facades\Cache;
-use Illuminate\Support\Facades\Redis;
 use Illuminate\Support\Facades\Route;
 
-
 Route::get('/', function () {
-    // $user1Stats = [
-    //     'favorites' => 90,
-    //     'watchLaters' => 50,
-    //     'completions' => 25,
-    // ];
-
-    // Redis::hmset('user.1.stats', $user1Stats);
-
-    // return Redis::hgetall('user.1.stats');
-
-    Cache::put('foo', ['name' => 'Laracasts', 'age' => 3], 10);
-
-    return Cache::get('foo');
-});
-
-Route::get('favorite-video', function () {
-    Redis::hincrby('user.1.stats', 'favorites', 1);
-
-    return redirect('/');
+    return Cache::remember('articles.all', 60 * 60, function () {
+        return Article::all();
+    });
 });
