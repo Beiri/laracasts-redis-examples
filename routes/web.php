f877dc51fe86ce80cff773ae483615a87602ef5c
Beiri Alife <beiri.al@outlook.com>
2024-09-23 19:04:54 -0300

    Ep 06 - How to Structure Your Caching Layer

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,44 @@
 <?php
 
 use App\Article;
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return Cache::remember('articles.all', 60 * 60, function () {
+interface Articles
+{
+    public function all();
+}
+
+class CacheableArticles implements Articles
+{
+    protected $articles;
+
+    public function __construct($articles)
+    {
+        $this->articles = $articles;
+    }
+
+    public function all()
+    {
+        return Cache::remember('articles.all', 60 * 60, function () {
+            return $this->articles->all();
+        });
+    }
+}
+
+class EloquentArticles implements Articles
+{
+    public function all()
+    {
         return Article::all();
-    });
+    }
+}
+
+App::bind('Articles', function () {
+    return new CacheableArticles(new EloquentArticles);
+});
+
+Route::get('/', function (Articles $articles) {
+    return $articles->all();
 });
